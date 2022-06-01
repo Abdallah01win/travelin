ed8b05a0bfbe251812b4c51fdfffa49df1856e05
Abdallah Bari <abdellah01win@gmail.com>
2022-06-01 15:43:09 +0100

    generate base files and add navigation

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<Navigation />
+<h1>Traveling Website</h1>
+</template>
+
+<script>
+import Navigation from "@/Components/Navigation.vue";
+export default {
+    components:{
+        Navigation,
+    }
+}
+</script>
