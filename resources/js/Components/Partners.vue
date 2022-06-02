bb093c7693bf0da00d5cbf47e3790175a0a1eec7
Abdallah Bari <abdellah01win@gmail.com>
2022-06-02 15:54:46 +0100

    build and style partners and stats sections

---
+++ resources/js/Components/Partners.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container mx-auto">
+    <ul class="flex flex-row justify-evenly align-center w-full py-20 /*h-fit*/">
+      <li>
+        <img src="/assets/partners/airbnb.png" alt="" class="max-h-12" />
+      </li>
+      <li>
+        <img src="/assets/partners/booking.png" alt="" class="max-h-12" />
+      </li>
+      <li>
+        <img src="/assets/partners/expedia.png" alt="" class="max-h-12" />
+      </li>
+      <li>
+        <img src="/assets/partners/hotels.png" alt="" class="max-h-12" />
+      </li>
+      <li>
+        <img src="/assets/partners/plaid.png" alt="" class="max-h-12" />
+      </li>
+    </ul>
+  </section>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,20 +1,24 @@
 <template>
   <Heading />
+  <Partners />
+  <StatsSection />
   <NewsLetter />
   <Footer />
 </template>
 
 <script>
-//import Navigation from "@/Components/Navigation.vue";
+import Partners from "@/Components/Partners.vue";
 import Heading from "@/Components/Heading.vue";
 import NewsLetter from "@/Components/NewsLetter.vue";
 import Footer from "@/Components/Footer.vue";
+import StatsSection from "@/Components/StatsSection.vue";
 export default {
   components: {
-    // Navigation,
+    Partners,
     Footer,
     NewsLetter,
     Heading,
+    StatsSection,
   },
 };
 </script>
