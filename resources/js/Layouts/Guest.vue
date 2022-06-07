6be03ea42a942ac23a4b2f80abfa61ed2bd071eb
Abdallah Bari <abdellah01win@gmail.com>
2022-06-07 20:14:12 +0100

    fix navigation not dynamicly styling bug

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -7,25 +7,35 @@
     </div>
 
     <div class="relative col-span-3">
-      <img src="/assets/volly-ball.jpg" alt="" class="h-screen w-full" />
-      <div
-        class="absolute bottom-0 left-0 bg-gray-100 bg-opacity-40 px-8 pt-10 pb-8 mx-10 mb-12 text-white backdrop-blur-sm">
-        <blockquote class="text-2xl mb-4">
-          "Ever since our first time, we've been using Travelin to plan every trip. Can't
-          imagine travelling without it."
-        </blockquote>
-        <div class="flex flex-row justify-between text-2xl font-semibold">
-          <div class="flex flex-col ">
-            <h3 class="">Andy Lane</h3>
-            <div class="text-base font-normal">Costarica</div>
-          </div>
-          <div>
-            <span v-for="i in 5" :key="i" class="">{{ '&#9733' }}</span>
-            <div class="flex flex-row gap-3 justify-center mt-1">
-              <span class="border-2 border-white py-0.75 px-1.5 rounded-full">{{ '&#8592' }}</span>
-              <span class="border-2 border-white py-0.75 px-1.5 rounded-full">{{ '&#8594' }}</span>
-            </div>
-          </div>
+      <img src="/assets/river.jpg" alt="" class="h-screen w-full" />
+      <div class="p-10 w-full absolute bottom-0 left-">
+        <div class="h-full bg-white p-8 rounded bg-opacity-40">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="block w-5 h-5 text-black mb-4"
+            viewBox="0 0 975.036 975.036"
+          >
+            <path
+              d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"
+            ></path>
+          </svg>
+          <p class="leading-relaxed mb-6 text-lg">
+            Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra
+            before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo,
+            post-ironic heirloom try-hard pabst authentic iceland.
+          </p>
+          <a class="inline-flex items-center">
+            <img
+              alt="testimonial"
+              src="https://dummyimage.com/106x106"
+              class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+            />
+            <span class="flex-grow flex flex-col pl-4">
+              <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
+              <span class="text-gray-900 text-sm">WEB DEVELOPER</span>
+            </span>
+          </a>
         </div>
       </div>
     </div>
