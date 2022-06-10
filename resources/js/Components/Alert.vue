9fdc230c7b7a39fe64821d57f029e4a5944528b7
Abdallah Bari <abdellah01win@gmail.com>
2022-06-10 22:10:47 +0100

    make alert comp reusable by adding props

--- resources/js/Components/Alert.vue
+++ resources/js/Components/Alert.vue
@@ -3,18 +3,17 @@
     class="bg-white w-1/3 mx-auto absolute top-[50%] translate-x-[50%] right-[50%] translate-y-[-50%] px-10 py-8"
   >
     <h3 class="text-2xl font-semibold text-gray-900 mb-4 text-center">
-      Delete Your Account?
+      {{ title }}
     </h3>
     <p></p>
     <p class="">
-      Are you sure you want to perminantly delete your account? This will delete any data
-      accosiated with this account including any uploads or saves you made.
+      {{ message }}
     </p>
     <div class="flex flex-row justify-between mt-4">
       <Inertia-link
         href="/userDelete"
         class="block text-white text-base hover:bg-red-700 bg-red-600 py-2 px-6 rounded-xl font-medium cursor-pointer"
-        >Delete</Inertia-link
+        >{{ action }}</Inertia-link
       >
       <a
         class="block text-white text-base hover:bg-green-700 bg-green-600 py-2 px-6 rounded-xl font-medium cursor-pointer"
@@ -29,10 +28,12 @@
 import { InertiaLink } from "@inertiajs/inertia-vue3";
 export default {
   Date() {
-    return {
-      /*title: String,
-      message: String,*/
-    };
+    return {};
+  },
+  props: {
+    title: String,
+    message: String,
+    action: String,
   },
   components: {
     InertiaLink,
