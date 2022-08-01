47b258e5262062e9f46f18197b208491be98a97b
Abdallah Bari <abdellah01win@gmail.com>
2022-08-01 13:15:09 +0100

    push users obj value into array to always make index 0

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,28 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use App\Models\Image;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $this->validate($request, [
             'name' => 'required|string',
             'adress' => 'required',
             'password' => Rules\Password::defaults(),
         ]);
+
+
         $input = [
             'name' =>  $request->name,
             'adress' =>  $request->adress,
             'password' =>  $request->password,
 
         ];
+        $imageName = time() . '.' . $request->file->extension();
+        $request->file->move(public_path('uploads'), $imageName);
         DB::table('users')
             ->where('id', Auth::id())
             ->update([
@@ -30,15 +37,21 @@ class UserController extends Controller
                 'adress' => $input['adress'],
                 'password' => Hash::make($input['password'])
             ]);
+
+        Image::create([
+            'userId' => Auth::id(),
+            'name' => $imageName
+        ]);
         return response()->json([
             "status" => true,
         ], 200);
     }
 
-    public function delete(){
+    public function delete()
+    {
         DB::table('users')
-        ->where('id', Auth::id())
-        ->delete();
+            ->where('id', Auth::id())
+            ->delete();
 
         return Inertia::render('Home');
     }
